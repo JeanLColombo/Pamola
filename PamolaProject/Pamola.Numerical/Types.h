3ddbad769fb581e0e8a288bb71c6fd06e07b3bde
Jean Luca Colombo <jeanlucacolombo@outlook.com>
2018-04-12 21:24:07 -0300

    Update Pamola Numerical.

--- PamolaProject/Pamola.Numerical/Types.h
+++ PamolaProject/Pamola.Numerical/Types.h
@@ -8,6 +8,4 @@ namespace SystemSolver
 	using oneVarFunc = std::function<double(double)>;
 	using multVarFunc = std::function<double(const std::vector<double> &)>;
 
-	//using Pmatrix = std::vector<std::vector<double>>;
-	using Pmatrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
 }
